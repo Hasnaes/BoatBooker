6c74cfafb6141f36ea4e024f91322fbfef0b545a
Leo Thompson <leo.francis.thompson@gmail.com>
2023-08-22 14:41:07 +0200

    index/add/show/delete of boats

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_091050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boats", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "capacity"
+    t.integer "price"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Boat.create(
+  name: 'Sample Boat 1',
+  description: 'A luxurious yacht for ocean adventures.',
+  capacity: 10,
+  price: 1000,
+  location: 'Miami, FL'
+)
+
+Boat.create(
+  name: 'Sample Boat 2',
+  description: 'A cozy sailboat for relaxing trips.',
+  capacity: 4,
+  price: 500,
+  location: 'San Francisco, CA'
+)
