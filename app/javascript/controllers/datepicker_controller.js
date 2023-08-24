1b641aa4105df5a0852f6413dd9f135caf51e1fa
Rohma Enggardini <rohmaenggardini@gmail.com>
2023-08-24 16:27:26 +0200

    did the display of the bookings

---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+// Connects to data-controller="datepicker"
+import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
+export default class extends Controller {
+  connect() {
+    flatpickr(this.element)
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :boats
+  resources :boats do
+    resources :bookings, only: [:create]
+  end
+  get "my_dashboard", to: "bookings#my_dashboard"
+
   root to: "pages#home"
   devise_for :users
+
   #  only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-
 end
